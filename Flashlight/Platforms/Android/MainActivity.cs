60648a8fb2cbc60cf4422d60c8a4d95884dfd8e2
popandepo <popandepo@gmail.com>
2022-07-18 13:21:32 +0200

    Imported flashlight functionality

--- Flashlight/Platforms/Android/MainActivity.cs
+++ Flashlight/Platforms/Android/MainActivity.cs
@@ -1,6 +1,9 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Flashlight.Pages;
 
 namespace Flashlight;
 
@@ -9,4 +12,19 @@ namespace Flashlight;
 	                       ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+	[Register("onKeyDown", "(ILandroid/view/KeyEvent;)Z", "GetOnKeyDown_ILandroid_view_KeyEvent_Handler")]
+	public override bool OnKeyDown(Keycode keyCode, KeyEvent e)
+	{
+		if (keyCode != Keycode.Power) return false;
+		FlashlightController.PowerButtonPressed = true;
+		return base.OnKeyDown(keyCode, e);
+	}
+	
+	[Register("onKeyUp", "(ILandroid/view/KeyEvent;)Z", "GetOnKeyUp_ILandroid_view_KeyEvent_Handler")]
+	public override bool OnKeyUp(Keycode keyCode, KeyEvent e)
+	{
+		if (keyCode != Keycode.Power) return false;
+		FlashlightController.PowerButtonPressed = false;
+		return base.OnKeyUp(keyCode, e);
+	}
 }
